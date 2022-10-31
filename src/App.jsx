26846e27bab2997ba13a82004403673c82e11894
Wesliane Silva Vieira <weslianebsb2@gmail.com>
2022-10-31 16:35:13 -0300

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,37 @@ import React, { useState } from 'react'
 import './styles.scss'
 import SearchInput from './components/SearchInput/SearchInput'
 import CurrentWeather from './components/CurrentWeather/CurrentWeather'
-import{ WEATHER_API_URL, WEATHER_API_KEY} from './services/resquests'
-import Forecast from './components/Forecast/Forecast'
+import { WEATHER_API_URL, WEATHER_API_KEY } from './services/resquests'
 
 const App = () => {
   const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
 
   // autocomplete busca
   const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(' ')
+    const [lat, lon] = searchData.value.split(" ")
 
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&lang=pt_br&units=metric`)
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}}&lang=pt_br&units=metric`); 
-  
-    Promise.all([currentWeatherFetch, forecastFetch])
+
+
+    Promise.all([currentWeatherFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
-        const forcastResponse = await response[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forcastResponse });
       })
-      .catch((err) => console.log(err));
+      .catch((err) => alert(err));
   }
 
   return (
     <div className='site-container'>
       <SearchInput onSearchChange={handleOnSearchChange} />
+      <div className='heading'>
+        <h1 className='heading-txt'>Weather for today</h1>
+        <span className='heading-subtxt'>Search on the bar above and get your results </span>
+      </div>
       {/* se exise mostrar dados*/}
-      { currentWeather && <CurrentWeather data={currentWeather}/>}
-      { forecast && <Forecast data={forecast}/>}
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   )
 }
-
 export default App
